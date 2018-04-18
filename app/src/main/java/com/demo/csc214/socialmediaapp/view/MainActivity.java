8c768bf813a06cbc426e567e6ff74540dcf2dead
saikaveti <saileshkaveti@hotmail.com>
2018-04-17 21:48:23 -0400

    Project Created

---
+++ app/src/main/java/com/demo/csc214/socialmediaapp/view/MainActivity.java
@@ -0,0 +1,15 @@
+package com.demo.csc214.socialmediaapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.demo.csc214.socialmediaapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
