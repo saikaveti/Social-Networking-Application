94a29412e0f9821c9372e05733b764134b512f9e
saikaveti <saileshkaveti@hotmail.com>
2018-04-19 23:00:36 -0400

    LoginActivity Done

---
+++ app/src/main/java/com/demo/csc214/socialmediaapp/controller/CheckLoginInputs.java
@@ -0,0 +1,44 @@
+package com.demo.csc214.socialmediaapp.controller;
+
+import android.widget.EditText;
+
+import com.demo.csc214.socialmediaapp.model.Database.UserDatabase;
+import com.demo.csc214.socialmediaapp.model.Entities.UserEntity;
+
+import java.util.List;
+
+/**
+ * Created by Sailesh on 4/19/18.
+ */
+
+public class CheckLoginInputs {
+
+    public static boolean validUserName(EditText userNameField, UserDatabase db) {
+        for (UserEntity user : db.userDAO().getAll()) {
+            if (user.getUsername().equals(userNameField.getText().toString())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean validAccount(EditText usernameField, EditText passwordField, UserDatabase db) {
+        for (UserEntity user : db.userDAO().getAll()) {
+            if (user.getUsername().equals(usernameField.getText().toString()) && user.getPassword().equals(passwordField.getText().toString())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static int getUserID(EditText usernameField, EditText passwordField, UserDatabase db) {
+        for (UserEntity user : db.userDAO().getAll()) {
+            if (user.getUsername().equals(usernameField.getText().toString()) && user.getPassword().equals(passwordField.getText().toString())) {
+                return user.getUser_id();
+            }
+        }
+        return 0;
+    }
+}
--- app/src/main/java/com/demo/csc214/socialmediaapp/view/LoginActivity.java
+++ app/src/main/java/com/demo/csc214/socialmediaapp/view/LoginActivity.java
@@ -1,15 +1,57 @@
 package com.demo.csc214.socialmediaapp.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.demo.csc214.socialmediaapp.R;
+import com.demo.csc214.socialmediaapp.controller.CheckLoginInputs;
+import com.demo.csc214.socialmediaapp.model.Database.UserDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText mUsernameField;
+    EditText mPasswordField;
+
+    Button mLoginButton;
+
+    public final String USERID_KEY = "USER_ID_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mUsernameField = findViewById(R.id.username_login_edit_text);
+
+        mPasswordField = findViewById(R.id.password_login_edit_text);
+
+        mLoginButton = findViewById(R.id.login_button_login_activity);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!CheckLoginInputs.validUserName(mUsernameField, UserDatabase.getInstance(getApplicationContext()))) {
+                    Toast.makeText(getApplicationContext(), "USERNAME INVALID", Toast.LENGTH_SHORT).show();
+                } else if (!CheckLoginInputs.validAccount(mUsernameField, mPasswordField, UserDatabase.getInstance(getApplicationContext()))) {
+                    Toast.makeText(getApplicationContext(), "USERNAME AND PASSWORD DO NOT MATCH", Toast.LENGTH_SHORT).show();
+                } else {
+                    int user_id = CheckLoginInputs.getUserID(mUsernameField, mPasswordField, UserDatabase.getInstance(getApplicationContext()));
+
+                    Intent myIntent = new Intent(LoginActivity.this, SocialActivity.class);
+
+                    myIntent.putExtra(USERID_KEY, user_id);
+
+                    startActivity(myIntent);
+                }
+            }
+        });
+
+
+
     }
 }
